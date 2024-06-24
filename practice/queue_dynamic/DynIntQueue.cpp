70a604531f02def61613df20d65c33f00c300ec6
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-25 01:12:22 +0800

    Practice dynamic queue

---
+++ practice/queue_dynamic/DynIntQueue.cpp
@@ -0,0 +1,60 @@
+#include "DynIntQueue.h"
+
+DynIntQueue::DynIntQueue() {
+  front = nullptr;
+  rear = nullptr;
+  numItems = 0;
+}
+
+DynIntQueue::~DynIntQueue() {
+  clear();
+}
+
+void DynIntQueue::enqueue (int num) {
+  QueueNode *newNode = nullptr;
+  newNode = new QueueNode;
+  newNode->value = num;
+  newNode->next = nullptr;
+
+  if (isEmpty()) {
+    front = newNode;
+    rear = newNode;
+  } else {
+    rear->next = newNode;
+    rear = newNode;
+  }
+
+  numItems++;
+}
+
+void DynIntQueue::dequeue (int & num) {
+  QueueNode *temp = nullptr;
+  
+  if (isEmpty())
+    cout << "The queue is empty.\n";
+  else {
+    num = front->value;
+    temp = front;
+    front = front->next;
+    delete temp;
+    numItems--;
+  }
+}
+
+bool DynIntQueue::isEmpty () const {
+  bool status;
+
+  if (numItems)
+    status = false;
+  else
+    status = true;
+
+  return status;
+}
+
+void DynIntQueue::clear () {
+  while (!isEmpty()) {
+    int catchNum;
+    dequeue(catchNum);
+  }
+}
