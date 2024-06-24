9fefa1d8a4664d95133ff3b3dc1900ce129884f6
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-25 00:51:33 +0800

    learn dynamic queue

---
+++ queue_dynamic_learn/DynIntQueue.h
@@ -0,0 +1,26 @@
+#ifndef DYNINTQUEUE_H
+#define DYNINTQUEUE_H
+
+#include <iostream>
+using namespace std;
+
+class DynIntQueue {
+  private:
+    struct QueueNode {
+      int value;
+      QueueNode *next;
+    };
+    QueueNode *front;
+    QueueNode *rear;
+    int numItems;
+  public:
+    DynIntQueue();
+    ~DynIntQueue();
+
+    void enqueue(int);
+    void dequeue(int &);
+    bool isEmpty() const;
+    void clear();
+};
+
+#endif
