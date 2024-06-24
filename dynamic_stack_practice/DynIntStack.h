f8057bfc28334e5102afa53bad4d3c33e0396e70
Lim Wen Hao <wenhaojshs@gmail.com>
2024-06-24 23:16:12 +0800

    practice dynamic stack

---
+++ dynamic_stack_practice/DynIntStack.h
@@ -0,0 +1,26 @@
+#ifndef DYNINTSTACK_H
+#define DYNINTSTACK_H
+
+#include <iostream>
+using namespace std;
+
+class DynIntStack {
+  private:
+    struct StackNode {
+      int val;
+      StackNode *next;
+    };
+    StackNode *top;
+  public:
+    DynIntStack() {
+      top = nullptr;
+    }
+    ~DynIntStack();
+
+    void push (int);
+    void pop (int &);
+
+    bool isEmpty ();
+};
+
+#endif
